be60adde7ee1ac404fa5c4578bce01f9e8bcbbbf
yhs-coder <yhs6666@outlook.com>
2023-12-07 21:02:45 +0800

    鼠标消息

---
+++ Window_programming/window_01/WinMouse/WinMouse.cpp
@@ -0,0 +1,123 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <windows.h>
+#include <stdio.h>
+
+HANDLE g_hOutput = 0; // 接收标准输出句柄
+
+void OnLButtonDown(HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+	char szText[256] = { 0 };
+	sprintf(szText, "WM_LButtonDown：其它按键状态:%d, X = %d，Y = %d\n", wParam,LOWORD(lParam),HIWORD(lParam));
+	WriteConsole(g_hOutput, szText, strlen(szText), NULL, NULL);
+}
+
+void OnLButtonUp(HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+	char szText[256] = { 0 };
+	sprintf(szText, "WM_LButtonUp：其它按键状态:%d, X = %d，Y = %d\n", wParam, LOWORD(lParam), HIWORD(lParam));
+	WriteConsole(g_hOutput, szText, strlen(szText), NULL, NULL);
+}
+
+void OnMouseMove(HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+	char szText[256] = { 0 };
+	sprintf(szText, "WM_MOUSEMOVE：其它按键状态:%d, X = %d，Y = %d\n", wParam, LOWORD(lParam), HIWORD(lParam));
+	// WriteConsole(g_hOutput, szText, strlen(szText), NULL, NULL);
+}
+
+void OnLButtonDblclk(HWND hWnd)
+{
+	char szText[] = "WM_LBUTTONDBLCLK\n";
+	WriteConsole(g_hOutput, szText, strlen(szText), NULL, NULL);
+}
+
+void OnMouseWheel(HWND hWnd, WPARAM wParam)
+{
+	short nDelta = HIWORD(wParam);	// 偏移量
+	char szText[256] = { 0 };
+	sprintf(szText, "WM_MOUSEWHEEL：nDelta:%d\n", wParam, nDelta);
+	WriteConsole(g_hOutput, szText, strlen(szText), NULL, NULL);
+}
+
+// 2. 窗口处理函数（自定义，处理消息）
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msgID, WPARAM wParam, LPARAM lParam) {
+	switch (msgID) {
+	// 鼠标滚轮滚动
+	case WM_MOUSEWHEEL:
+		OnMouseWheel(hWnd, wParam);
+		break;
+	// 左键双击
+	case WM_LBUTTONDBLCLK:
+		OnLButtonDblclk(hWnd);
+		break;
+	// 移动鼠标
+	case WM_MOUSEMOVE:
+		OnMouseMove(hWnd, wParam, lParam);
+		break;
+	// 点击鼠标左键
+	case WM_LBUTTONDOWN:
+		OnLButtonDown(hWnd, wParam, lParam);
+		break;
+	// 松开鼠标左键
+	case WM_LBUTTONUP:
+		OnLButtonUp(hWnd, wParam, lParam);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0); // 可以使GetMessage函数返回0？
+		break;
+	case WM_SYSCOMMAND:
+		// MessageBox(hWnd, "WM_SYSCOMMAND", "提示框", MB_OK);
+		// 点击窗口关闭按钮
+		if (wParam == SC_CLOSE) {
+			int nRet = MessageBox(hWnd, "是否退出", "Information", MB_YESNO);
+			if (nRet == IDNO) {
+				char szText[256] = "点击了否\n";
+				WriteConsole(g_hOutput, szText, strlen(szText), NULL, NULL);
+				return 0;
+			}
+		}
+		break;
+	}
+	return DefWindowProc(hWnd, msgID, wParam, lParam); // 给各种消息做默认处理
+}
+
+
+// 1.定义WinMain入口函数
+int CALLBACK WinMain(HINSTANCE hIns, HINSTANCE hPerIns, LPSTR IpCmdLine, int nCmdShow) {
+	AllocConsole(); // 增加dos窗口
+	g_hOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	// 3. 注册窗口类 （向系统内核写入数据）
+	WNDCLASS wc = { 0 };
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wc.hCursor = NULL;
+	wc.hIcon = NULL;
+	wc.hInstance = hIns;
+	wc.lpfnWndProc = WndProc;
+	wc.lpszClassName = "Main";
+	wc.lpszMenuName = NULL;
+	wc.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
+	RegisterClass(&wc);
+
+	const char* pszText = "hello data";
+	HWND hWnd = CreateWindow("Main", "window", WS_OVERLAPPEDWINDOW, 100, 100, 500, 500, NULL, NULL, hIns, (LPVOID)pszText);
+
+	if (hWnd == NULL) {
+		MessageBox(NULL, "窗口创建失败！", "错误", MB_ICONERROR);
+		return 0;
+	}
+
+	// 5.显示窗口 (在显示器中绘制窗口的图像)
+	ShowWindow(hWnd, SW_SHOW);
+	UpdateWindow(hWnd); // 刷新窗口
+
+	// 6. 消息循环
+	MSG nMsg = { 0 };
+	while (GetMessage(&nMsg, NULL, 0, 0)) {
+		TranslateMessage(&nMsg);
+		DispatchMessage(&nMsg);	// 将消息交给窗口处理函数处理
+	}
+	return 0;
+}
