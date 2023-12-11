43870f6a6a98bb000c59f202afc8d60d26a3e096
yhs-coder <yhs6666@outlook.com>
2023-12-11 22:22:58 +0800

    UDP服务端

---
+++ Window_programming/WinidowsNetworkProgramming/UDPClient/UDPClient.cpp
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <winsock2.h>
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#pragma comment(lib, "ws2_32.lib")
+
+int main()
+{
+	// 初始化套接字库
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+		cout << "WSAStartup err：" << GetLastError() << endl;
+		return -1;
+	}
+
+	if (LOBYTE(wsa.wVersion) != 2 || HIBYTE(wsa.wVersion) != 2) {
+		cout << "LOBYTE or BIBYTE error:" << GetLastError() << endl;
+		WSACleanup();
+		return -1;
+	}
+	cout << "this is UDP client..." << endl;
+
+
+	SOCKET sockClt = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sockClt == INVALID_SOCKET) {
+		cout << "socket error: " << GetLastError()
+			<< endl;
+		return -1;
+	}
+
+	// 目标套接字地址信息
+	SOCKADDR_IN addrSrv;
+	addrSrv.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+	addrSrv.sin_family = AF_INET;
+	addrSrv.sin_port = htons(10001);
+	int len = sizeof(addrSrv);
+	// udp通信
+	char recvBuf[256] = { 0 };
+	char sendBuf[256] = "hello world!";
+
+	// UDP通信
+	
+	// 发送数据
+	sendto(sockClt, sendBuf, strlen(sendBuf) + 1, 0, (SOCKADDR*)&addrSrv, len);
+
+	// 接收数据
+	recvfrom(sockClt, recvBuf, sizeof(recvBuf), 0, (SOCKADDR*)&addrSrv, &len);
+	cout << recvBuf << endl;
+
+
+	closesocket(sockClt);
+	WSACleanup();
+	system("pause");
+	return 0;
+}
+
+
