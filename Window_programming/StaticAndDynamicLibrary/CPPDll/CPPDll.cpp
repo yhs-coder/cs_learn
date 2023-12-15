4a6177e2100910430e4bfd26904c5e0dbd20edef
yhs-coder <yhs6666@outlook.com>
2023-12-15 23:39:41 +0800

    制作动态库

--- Window_programming/StaticAndDynamicLibrary/CPPDll/CPPDll.cpp
+++ Window_programming/StaticAndDynamicLibrary/CPPDll/CPPDll.cpp
@@ -1,20 +1,45 @@
-
-int cpplib_add(int a, int b)
+#if 0
+// 使用声明导出 _declspec(dllexport)
+_declspec(dllexport) int cppdll_add(int a, int b)
 {
 	return a + b;
 }
 
-int cpplib_sub(int a, int b)
+_declspec(dllexport)  int cppdll_sub(int a, int b)
 {
 	return a - b;
 }
 
-int cpplib_mul(int a, int b)
+_declspec(dllexport)  int cppdll_mul(int a, int b)
 {
 	return a * b;
 }
 
-int cpplib_div(int a, int b)
+_declspec(dllexport)  int cppdll_div(int a, int b)
 {
 	return a / b;
 }
+
+#else
+// 模块定义文件
+int cppdll_add(int a, int b)
+{
+	return a + b;
+}
+
+int cppdll_sub(int a, int b)
+{
+	return a - b;
+}
+
+int cppdll_mul(int a, int b)
+{
+	return a * b;
+}
+
+int cppdll_div(int a, int b)
+{
+	return a / b;
+}
+
+#endif
